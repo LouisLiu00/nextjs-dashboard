56da5e3f23cb4ecdc369a51627bb611cabda6d2a
Louis <louis.liu.oneself@hotmail.com>
2024-11-27 15:16:17 +0800

    初始化上传项目

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Lexend_Deca } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const lexend_deca = Lexend_Deca({ subsets: ['latin'] });
