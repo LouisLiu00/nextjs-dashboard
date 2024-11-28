8765247b4d8a69324b16bfc8b289eefb324f10e0
Louis <louis.liu.oneself@hotmail.com>
2024-11-28 11:32:25 +0800

    更新元数据

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,17 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: {
     template: '%s | Dashboard',
-    default: 'Dashboard',
+    default: ' Dashboard',
   },
-  description: 'The official Next.js  Dashboard, built with App Router.',
-  metadataBase: new URL('https://nextjs-dashboard-flame-alpha.vercel.app'),
-  keywords: ['Next.js', 'React', 'JavaScript', 'Louis'],
-  creator: 'Louis Liu',
+  description: 'The official Next.js Course Dashboard, built with App Router. {Louis Liu (@LouisLiuOneself)}',
+  keywords: ['Next.js', 'React', 'JavaScript', 'Louis Liu (@LouisLiuOneself)'],
+  creator: 'Louis Liu (@LouisLiuOneself)',
+  publisher: 'Louis Liu (@LouisLiuOneself)',
+  authors: [{ name: 'Louis Liu (@LouisLiuOneself)', url: 'https://github.com/LouisLiu00' }],
+  openGraph: {
+    title: 'Dashboard',
+    description: 'The official Next.js Course Dashboard, built with App Router. {Louis Liu (@LouisLiuOneself)}',
+  },
 };
 
 export default function RootLayout({
